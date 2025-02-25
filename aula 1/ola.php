01bb1ba23034e11341447b0cfa7d4761781d533f
Gustavo Silva <gustavomascio12@gmail.com>
2025-02-25 16:34:11 -0300

    Aula de PHP com exemplos de concatenação de variaveis

---
+++ aula 1/ola.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+        <?php
+            echo "Ola Mundo <br>";
+                $nome = "Gu";
+                $sobrenome = "Mascio";
+            $idade = 16;
+                echo "${nome} <br>";
+            echo $nome . " " . $sobrenome . " " . $idade;
+            echo "<h1> ${nome}  {$sobrenome} </h1>";
+// crtl e ; para fazer comentario
+        ?>
+             
+        
+    </body>
+</html>
