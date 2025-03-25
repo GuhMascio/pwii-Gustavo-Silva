6ee343de47a649fba2f6c28a00e68becb0760b51
Gustavo Mascio <gustavomascio12@gmail.com>
2025-03-25 16:31:01 -0300

    Aula sobre concatenação

---
+++ aula 1/aula2/concatenar.php
@@ -0,0 +1,14 @@
+<?php
+    ECHO "Hello world! <br>";
+    echo "Hello world! <br>";
+    EcHo "Hello world! <br>";
+
+    $nome = "Gustavo";
+    $sobrenome = "Mascio";
+    $numero = 16;
+
+    echo $nome . "lindo" . "<br>";
+    echo "${nome} <h1> ${sobrenome} </h1>";
+    echo $numero . "<p> " . $nome .
+    " " . $sobrenome . "</p>";
+?>
