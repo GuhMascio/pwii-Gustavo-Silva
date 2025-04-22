c3b248210296f242ecdac958e40fa981f2dd2ae1
Gustavo Silva <gustavomascio12@gmail.com>
2025-04-22 16:37:00 -0300

    Aula sobre If

---
+++ aula 3/decisao.php
@@ -0,0 +1,25 @@
+<?php
+    $x =5;
+    $y = 9;
+    $media - ($x + $y) / 2;
+    //media >= 6,0 = 'aprovado'
+    //media <6,0 && >= 4,0
+    //media = 'recuperação'
+    //media < 4,0 = 'reprovado'
+
+    if ($media >= 6.0)
+    {
+        //se aprovado
+        echo 'Aluno aprovado';
+    }
+    else if ($media >4.0)
+    {
+        //se recuperação
+        echo 'Aluno em recuperação';
+    }
+    else 
+    {
+        //se reprovado
+        echo 'Aluno reprovado';            
+    }
+?>
